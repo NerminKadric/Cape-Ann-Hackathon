fbd1b1e8af8c3563cca8893ec59deadaa30b0bab
nerminkadric <nerminkadric54@gmail.com>
2021-09-14 18:18:27 +0200

    Added dropdown menu fi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
-   
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Select } from './formSelection.model';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+
+  
+  selectedDay: string = '';
   constructor() { }
 
   ngOnInit(): void {
+;
+  }
+  selectChangeHandler (event: any) {
+    //update the ui
+    this.selectedDay = event.target.value;
+    console.log(event.target.value)
   }
 
 }
+
+
+
+
+
+
+
